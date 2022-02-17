400ba81e506a6b44e5d99928fd5ece203fc58147
FUCK OFF ANE <98939932+TEAM-FLUFFY@users.noreply.github.com>
2022-02-17 15:48:23 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -15,11 +15,6 @@ from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup, InputMedi
 @Client.on_message(filters.private & filters.command(['start']))
 async def start(client, message):
     buttons = [[
-        InlineKeyboardButton('📜 Support Group', url='https://t.me/Mo_Tech_Group'),
-        InlineKeyboardButton('Update Channel ♻️', url='https://t.me/Mo_Tech_YT')
-        ],[
-        InlineKeyboardButton('💡 Deploy', url='https://youtu.be/B6jQGOJDVi0'),
-        InlineKeyboardButton('String Session 🎻', url ='https://youtu.be/WUN_12-dYOM')
     ]]
     reply_markup = InlineKeyboardMarkup(buttons)
     await client.send_message(
@@ -34,8 +29,6 @@ async def start(client, message):
 @Client.on_message(filters.private & filters.command(['help']))
 async def help(client, message):
     buttons = [[
-        InlineKeyboardButton('SouceCode 💡', url='https://github.com/Jijinr/Frwdit-V2'),
-        InlineKeyboardButton('close 🔐', callback_data='close_btn')
     ]]
     reply_markup = InlineKeyboardMarkup(buttons)
     await client.send_message(
@@ -49,7 +42,6 @@ async def help(client, message):
 @Client.on_message(filters.private & filters.command(['about']))
 async def about(client, message):
     buttons = [[
-        InlineKeyboardButton('💡 Deploy', url='https://github.com/Jijinr/Frwdit-V2'),
         InlineKeyboardButton('close 🔐', callback_data='close_btn')
     ]]
     reply_markup = InlineKeyboardMarkup(buttons)
